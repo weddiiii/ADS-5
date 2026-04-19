a758c9fd1367c21851373055320004d720c39a29
weddiiii <150699840+weddiiii@users.noreply.github.com>
2026-04-19 16:56:19 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -2,9 +2,45 @@
 #ifndef INCLUDE_TSTACK_H_
 #define INCLUDE_TSTACK_H_
 
+#include <stdexcept>
+
 template<typename T, int size>
 class TStack {
-  // добавьте код стека
+ private:
+    T data[size];
+    int topIndex;
+
+ public:
+    TStack() : topIndex(-1) {}
+
+    bool isEmpty() const {
+        return topIndex == -1;
+    }
+
+    bool isFull() const {
+        return topIndex == size - 1;
+    }
+
+    void push(const T& value) {
+        if (isFull()) {
+            throw std::overflow_error("Stack overflow");
+        }
+        data[++topIndex] = value;
+    }
+
+    void pop() {
+        if (isEmpty()) {
+            throw std::underflow_error("Stack underflow");
+        }
+        --topIndex;
+    }
+
+    T top() const {
+        if (isEmpty()) {
+            throw std::underflow_error("Stack is empty");
+        }
+        return data[topIndex];
+    }
 };
 
 #endif  // INCLUDE_TSTACK_H_
